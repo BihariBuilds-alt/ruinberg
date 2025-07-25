5059f3f495a8fb3f0de4b1061ebf541e81585b1d
BihariBuilds-alt <babankumarkarn@gmail.com>
2025-07-25 13:15:54 +0530

    Create App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Navigation from './components/Navigation';
+
+function App() {
+  return (
+    <div className="app">
+      <Navigation />
+      <h1>Welcome to Ruinberg</h1>
+    </div>
+  );
+}
+
+export default App;
